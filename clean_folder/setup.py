8bc4b0607af4fd2420719759c4d215502950ed0c
Сергій Резванюк <133152778+SergRezvanyuk@users.noreply.github.com>
2023-05-28 19:19:52 +0300

    Summary

---
+++ clean_folder/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='clean_folder',
+    version='1.1.3',
+    description='T.7 Clean_folder',
+    url='http://github.com/dummy_user/useful',
+    author='Sergii Rezvaniuk',
+    author_email='o_serg@ukr.net',
+    license='MIT',
+    packages=find_namespace_packages(),
+    entry_points={'console_scripts': ['clean-folder=clean_folder.clean:main']}
+)
